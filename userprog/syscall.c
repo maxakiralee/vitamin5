2e264d25b4401e8c2f92932b09bd748ce0c3f5d7
Max Lee <maxlee10@g.ucla.edu>
2025-05-23 23:39:48 +0000

    vitamin 5 setup

---
+++ userprog/syscall.c
@@ -0,0 +1,50 @@
+#include "userprog/syscall.h"
+
+#include <stdio.h>
+#include <syscall-nr.h>
+
+#include "threads/interrupt.h"
+#include "threads/thread.h"
+
+#include "lib/kernel/stdio.h"
+
+static void syscall_handler(struct intr_frame *);
+
+void syscall_init(void) {
+    intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+static void syscall_handler(struct intr_frame *f UNUSED) {
+    uint32_t *args = ((uint32_t *) f->esp);
+
+    /*
+     * The following print statement, if uncommented, will print out the syscall
+     * number whenever a process enters a system call. You might find it useful
+     * when debugging. It will cause tests to fail, however, so you should not
+     * include it in your final submission.
+     */
+
+    /* printf("System call number: %d\n", args[0]); */
+
+    if (args[0] == SYS_EXIT) {
+        f->eax = args[1];
+        printf("%s: exit(%d)\n", thread_current()->name, args[1]);
+        thread_exit();
+    // Part 3
+    } else if (args[0] == SYS_INCREMENT) {
+        int i = args[1];
+        f->eax = i + 1;
+    // Part 4
+    } else if (args[0] == SYS_WRITE) {
+        int fd = args[1];
+        const void *buf = (const void *) args[2];
+        unsigned size = args[3];
+
+        if (fd == 1) {    
+            putbuf (buf, size);             
+            f->eax = size;                  
+        } else {
+            f->eax = -1;                    
+        }
+    }
+}
