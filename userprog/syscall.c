7b840a6e1146bc69a13e3d353ba48f10b96d5b04
Jonathan Si <jonathansi@g.ucla.edu>
2025-05-30 16:28:56 -0700

    added checker functionality

--- userprog/syscall.c
+++ userprog/syscall.c
@@ -5,6 +5,7 @@
 
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 #include "threads/synch.h"
 #include "filesys/filesys.h"
 #include "filesys/file.h"
@@ -12,8 +13,37 @@
 #include "lib/kernel/stdio.h"
 
 static void syscall_handler(struct intr_frame *);
+static int get_user (const uint8_t *uaddr);
+static bool put_user (uint8_t *udst, uint8_t byte);
+static void check_valid_ptr(const void *ptr); 
+
 static struct lock filesys_lock;
 
+/* Reads a byte at user virtual address UADDR.
+UADDR must be below PHYS_BASE.
+Returns the byte value if successful, -1 if a segfault
+occurred. */
+static int get_user (const uint8_t *uaddr){
+    int result;
+    asm ("movl $1f, %0; movzbl %1, %0; 1:"
+        : "=&a" (result) : "m" (*uaddr));
+    return result;
+}
+/* Writes BYTE to user address UDST.
+UDST must be below PHYS_BASE.
+Returns true if successful, false if a segfault occurred. */
+static bool put_user (uint8_t *udst, uint8_t byte){
+    int error_code;
+    asm ("movl $1f, %0; movb %b2, %1; 1:" 
+        : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+    return error_code != -1;
+}
+
+static void check_valid_ptr(const void *ptr) {
+    if (ptr == NULL || !is_user_vaddr(ptr) || ptr < (void *)0x08048000) {
+        thread_exit();
+    }
+}
 
 void syscall_init(void) {
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
@@ -22,6 +52,8 @@ void syscall_init(void) {
 
 static void syscall_handler(struct intr_frame *f UNUSED) {
     uint32_t *args = ((uint32_t *) f->esp);
+    check_valid_ptr(args);
+
 
     /*
      * The following print statement, if uncommented, will print out the syscall
